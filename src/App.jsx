b6e4643e2594248c52ddff2452a255e215cbf4d1
chadhas282-afk <chadhas282@gmail.com>
2026-06-19 10:52:15 +0530

    Add initial imports for main application components

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import CanvasArea from './components/CanvasArea';
+import Timeline from './components/Timeline';
