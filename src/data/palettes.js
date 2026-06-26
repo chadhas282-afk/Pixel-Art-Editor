ed2d47ec7da2a9780bb9e039f203918c3be41178
chadhas282-afk <chadhas282@gmail.com>
2026-06-26 09:57:08 +0530

    Add built-in PICO-8 color palette to palettes data

---
+++ src/data/palettes.js
@@ -0,0 +1,12 @@
+export const BUILT_IN_PALETTES = [
+  {
+    id: 'pico8',
+    name: 'PICO-8',
+    colors: [
+      '#000000','#1d2b53','#7e2553','#008751',
+      '#ab5236','#5f574f','#c2c3c7','#fff1e8',
+      '#ff004d','#ffa300','#ffec27','#00e436',
+      '#29adff','#83769c','#ff77a8','#ffccaa',
+    ],
+  },
+  {
