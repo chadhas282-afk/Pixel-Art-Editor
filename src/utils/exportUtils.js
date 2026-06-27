a2f961edd73be45cdc820c886191bb439cb5bdfe
chadhas282-afk <chadhas282@gmail.com>
2026-06-27 10:06:08 +0530

    Add exportFramePNG function to create a canvas for exporting frames as PNG

--- src/utils/exportUtils.js
+++ src/utils/exportUtils.js
@@ -4,3 +4,11 @@ export const download = (url, name) => {
   a.download = name;
   a.click();
 };
+
+export const exportFramePNG = (frames, currentFrameIndex, gridSize, scale, bgColor) => {
+  const tmp = document.createElement('canvas');
+  tmp.width = gridSize * scale; 
+  tmp.height = gridSize * scale;
+  const ctx = tmp.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
+  
