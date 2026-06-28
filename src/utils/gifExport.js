77ce9fa3502497bfe9e6d7a6a53eb1f50502848d
chadhas282-afk <chadhas282@gmail.com>
2026-06-28 10:11:08 +0530

    Add emit and reset functions to lzwEncode for improved encoding logic

--- src/utils/gifExport.js
+++ src/utils/gifExport.js
@@ -6,3 +6,10 @@ function lzwEncode(indexStream, colorDepth) {
   const table = new Map();
   const output = [];
   let buf = 0, bufBits = 0;
+  const emit = (code) => {
+    buf |= code << bufBits;
+    bufBits += codeSize;
+    while (bufBits >= 8) { output.push(buf & 0xff); buf >>= 8; bufBits -= 8; }
+  };
+  const reset = () => {
+    table.clear();
